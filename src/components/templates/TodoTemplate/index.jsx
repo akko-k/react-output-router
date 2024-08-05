05cd6044ba212829f8a26ec2f703f97469f5866c
kawakami <barcarolle.op60.horowitz@gmail.com>
2024-08-05 23:41:37 +0900

    リストを定数化

--- src/components/templates/TodoTemplate/index.jsx
+++ src/components/templates/TodoTemplate/index.jsx
@@ -1,34 +1,39 @@
 import styles from './styles.module.css';
+import { useState } from 'react';
 
 export const TodoTemplate = () => {
-
+  const INIT_TODO_LIST = [
+    {
+      id: 1,
+      title: 'ご飯を作る',
+    },
+    {
+      id: 2,
+      title: '洗濯する',
+    },
+  ];
+  const [todos, setTodos] = useState(INIT_TODO_LIST);
   return (
-      <div className={styles.container}>
-    <h1>TODO List</h1>
-    <div>ADD TODO</div>
-    {/* リスト追加 */}
-    <div>
-      <input type="text" />
+    <div className={styles.container}>
+      <h1>TODO List</h1>
+      <div>ADD TODO</div>
+      {/* リスト追加 */}
+      <div>
+        <input type="text" />
+      </div>
+      {/* 検索 */}
+      <div>
+        <input type="text" />
+      </div>
+      {/* リスト */}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.title}</span>
+            <button>削除</button>
+          </li>
+        ))}
+      </ul>
     </div>
-    {/* 検索 */}
-    <div>
-      <input type="text" />
-    </div>
-    {/* リスト */}
-    <ul>
-      <li>
-        <span>TODO1</span>
-        <button>削除</button>
-      </li>
-      <li>
-        <span>TODO2</span>
-        <button>削除</button>
-      </li>
-      <li>
-        <span>TODO3</span>
-        <button>削除</button>
-      </li>
-    </ul>
-  </div>
-);
-}
+  );
+};
