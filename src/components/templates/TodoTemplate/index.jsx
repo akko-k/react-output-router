bd9438aa4ea81ac54167b9dcb1da734493d2c426
kawakami <barcarolle.op60.horowitz@gmail.com>
2024-08-06 23:10:11 +0900

    新規TODO追加処理を実装

--- src/components/templates/TodoTemplate/index.jsx
+++ src/components/templates/TodoTemplate/index.jsx
@@ -12,20 +12,47 @@ export const TodoTemplate = () => {
       title: '洗濯する',
     },
   ];
-    const [todos, setTodos] = useState(INIT_TODO_LIST);
+
+  const [todos, setTodos] = useState(INIT_TODO_LIST);
+  const [addInputValue, setAddInputValue] = useState('');
+
+  //新規リスト入力
+  const onChangeAddInputValue = (e) => setAddInputValue(e.target.value);
+  //新規リスト追加
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && addInputValue) {
+      const newTodo = [
+        ...todos,
+        {
+          id: todos.length + 1,
+          title: addInputValue,
+        },
+      ];
+      setTodos(newTodo);
+      setAddInputValue('');
+    }
+  };
+
   return (
     <div className={styles.container}>
       <h1>TODO List</h1>
 
       <div>
         {/* リスト追加 */}
-        <div>ADD TODO</div>
-        <input type="text" />
-        <button>追加</button>
+        <h2>新規追加</h2>
+        <input
+          // className={}
+          type="text"
+          placeholder={'TODOを入力'}
+          value={addInputValue}
+          onChange={onChangeAddInputValue}
+          onKeyDown={handleKeyDown}
+        />
       </div>
 
       <div>
         {/* 検索 */}
+        <h2>検索</h2>
         <input type="text" />
         <button>検索</button>
       </div>
