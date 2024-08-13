6ab5d0f7020f48d97054c92908d54549ebce6e80
kawakami <barcarolle.op60.horowitz@gmail.com>
2024-08-13 22:27:47 +0900

    カスタムフックを追加

--- src/components/templates/TodoTemplate/index.jsx
+++ src/components/templates/TodoTemplate/index.jsx
@@ -1,12 +1,9 @@
 import styles from './styles.module.css';
-import { useState, useMemo } from 'react';
-import {
-  INIT_TODO_LIST,
-  INIT_UNIQUE_ID,
-} from '../../../constants/data';
+
 import { TodoList } from '../../organisms/TodoList';
 import { AddTodo } from '../../organisms/AddTodo';
 import { SearchForm } from '../../atoms/SearchForm';
+import { useTodo } from '../../../hooks/useTodo';
 
 /**
  * TodoTemplate component.
@@ -15,56 +12,15 @@ import { SearchForm } from '../../atoms/SearchForm';
  * @returns {JSX.Element} TodoTemplate component.
  */
 export const TodoTemplate = () => {
-  //todoリスト
-  const [todos, setTodos] = useState(INIT_TODO_LIST);
-  //採番ID
-  const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
-  //新規TODOリスト入力値
-  const [addInputValue, setAddInputValue] = useState('');
-  //検索ワード
-  const [searchKeyword, setSearchKeyword] = useState('');
-
-  //新規TODOリスト入力値を更新
-  const onChangeAddInputValue = (e) =>
-    setAddInputValue(e.target.value);
-
-  //検索ワードに一致するTODOリストを取得
-  const filteredTodos = useMemo(() => {
-    return todos.filter((todo) =>
-      todo.title
-        .toLowerCase()
-        .includes(searchKeyword.toLowerCase())
-    );
-  }, [todos, searchKeyword]);
-
-  //TODOリストに新規TODOリストを追加
-  const handleAddNewTodo = () => {
-    if (addInputValue) {
-      const nextUniqueId = uniqueId + 1;
-      const newTodo = [
-        ...todos,
-        {
-          id: nextUniqueId,
-          title: addInputValue,
-        },
-      ];
-      setUniqueId(nextUniqueId);
-      setTodos(newTodo);
-      setAddInputValue('');
-    }
-  };
-
-  //TODOリストを削除
-  const handleDeleteTodo = (id) => {
-    const newTodo = todos.filter(
-      (todo, index) => index !== id
-    );
-    setTodos(newTodo);
-  };
-  // 検索ワードを更新
-  const handleChangeSearchKeyword = (e) =>
-    setSearchKeyword(e.target.value);
-
+  const {
+    addInputValue,
+    searchKeyword,
+    onChangeAddInputValue,
+    filteredTodos,
+    handleAddNewTodo,
+    handleDeleteTodo,
+    handleChangeSearchKeyword,
+  } = useTodo();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>TODO List</h1>
---
+++ src/hooks/useTodo.js
@@ -0,0 +1,70 @@
+import { useState, useMemo } from 'react';
+import {
+  INIT_TODO_LIST,
+  INIT_UNIQUE_ID,
+} from '../constants/data';
+
+/**
+ * use Todo
+ */
+export const useTodo = () => {
+  //todoリスト
+  const [todos, setTodos] = useState(INIT_TODO_LIST);
+  //採番ID
+  const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
+  //新規TODOリスト入力値
+  const [addInputValue, setAddInputValue] = useState('');
+  //検索ワード
+  const [searchKeyword, setSearchKeyword] = useState('');
+
+  //新規TODOリスト入力値を更新
+  const onChangeAddInputValue = (e) =>
+    setAddInputValue(e.target.value);
+
+  //検索ワードに一致するTODOリストを取得
+  const filteredTodos = useMemo(() => {
+    return todos.filter((todo) =>
+      todo.title
+        .toLowerCase()
+        .includes(searchKeyword.toLowerCase())
+    );
+  }, [todos, searchKeyword]);
+
+  //TODOリストに新規TODOリストを追加
+  const handleAddNewTodo = () => {
+    if (addInputValue) {
+      const nextUniqueId = uniqueId + 1;
+      const newTodo = [
+        ...todos,
+        {
+          id: nextUniqueId,
+          title: addInputValue,
+        },
+      ];
+      setUniqueId(nextUniqueId);
+      setTodos(newTodo);
+      setAddInputValue('');
+    }
+  };
+
+  //TODOリストを削除
+  const handleDeleteTodo = (id) => {
+    const newTodo = todos.filter(
+      (todo, index) => index !== id
+    );
+    setTodos(newTodo);
+  };
+  // 検索ワードを更新
+  const handleChangeSearchKeyword = (e) =>
+    setSearchKeyword(e.target.value);
+
+  return {
+    addInputValue,
+    searchKeyword,
+    onChangeAddInputValue,
+    filteredTodos,
+    handleAddNewTodo,
+    handleDeleteTodo,
+    handleChangeSearchKeyword,
+  };
+};
