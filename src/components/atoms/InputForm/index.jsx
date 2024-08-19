ad378063263deb502e5aef61e2e4c76f57ed76ce
kawakami <barcarolle.op60.horowitz@gmail.com>
2024-08-19 22:56:28 +0900

    inputとtextareaの共通コンポーネントを作成

---
+++ src/components/atoms/InputForm/index.jsx
@@ -0,0 +1,20 @@
+import style from './style.module.css';
+export const InputForm = ({
+  value,
+  disabled = false,
+  placeholder,
+  onChange,
+}) => {
+  return (
+    <div>
+      <textarea
+        className={style.input}
+        type="text"
+        value={value}
+        disabled={disabled}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
---
+++ src/components/atoms/TextArea/index.jsx
@@ -0,0 +1,20 @@
+import style from './style.module.css';
+export const TextArea = ({
+  value,
+  disabled = false,
+  placeholder,
+  onChange,
+}) => {
+  return (
+    <div>
+      <input
+        className={style.text}
+        type="text"
+        value={value}
+        disabled={disabled}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
